7d5c2547917a762f09b64dc02e022b64706815ae
Your Name <you@example.com>
2021-09-08 08:29:32 +0900

    fix skyway api caller.

--- lib/skyway_wrapper.dart
+++ lib/skyway_wrapper.dart
@@ -2,7 +2,6 @@ import 'package:js/js.dart';
 import 'package:uuid/uuid.dart';
 import 'skyway_stab.dart' as stab;
 
-
 @JS('newPeer')
 external void _newPeer(String key, int debug, Function(String) onOpenCallback);
 
@@ -23,16 +22,18 @@ external void _sendData(String data);
 @JS('leaveRoom')
 external void _leaveRoom();
 
-
 class SkyWayHelper {
-  SkyWayHelper({this.useStab=false});
+  SkyWayHelper({this.useStab = false});
+
   final bool useStab;
   final peerId = const Uuid().v4();
 
   void newPeer(String key, int debug, Function(String) onOpenCallback) {
-    final func = useStab ? stab.newPeer : _newPeer;
-    // func(peerId, key, debug, allowInterop(onOpenCallback));
-    func(key, debug, allowInterop(onOpenCallback));
+    if (useStab) {
+      stab.newPeer(peerId, key, debug, allowInterop(onOpenCallback));
+    } else {
+      _newPeer(key, debug, allowInterop(onOpenCallback));
+    }
   }
 
   void joinRoom(
@@ -44,37 +45,44 @@ class SkyWayHelper {
       Function(String, String) onDataCallback,
       Function(String) onPeerLeave,
       Function() onClose) {
-    final func = useStab ? stab.joinRoom : _joinRoom;
-    /*
-    func(
-        peerId,
-        roomId,
-        mode,
-        allowInterop(onOpenCallback),
-        allowInterop(onPeerJoinCallback),
-        allowInterop(onStreamCallback),
-        allowInterop(onDataCallback),
-        allowInterop(onPeerLeave),
-        allowInterop(onClose));
-     */
-    func(
-        roomId,
-        mode,
-        allowInterop(onOpenCallback),
-        allowInterop(onPeerJoinCallback),
-        allowInterop(onStreamCallback),
-        allowInterop(onDataCallback),
-        allowInterop(onPeerLeave),
-        allowInterop(onClose));
+    if (useStab) {
+      stab.joinRoom(
+          peerId,
+          roomId,
+          mode,
+          allowInterop(onOpenCallback),
+          allowInterop(onPeerJoinCallback),
+          allowInterop(onStreamCallback),
+          allowInterop(onDataCallback),
+          allowInterop(onPeerLeave),
+          allowInterop(onClose));
+    } else {
+      _joinRoom(
+          roomId,
+          mode,
+          allowInterop(onOpenCallback),
+          allowInterop(onPeerJoinCallback),
+          allowInterop(onStreamCallback),
+          allowInterop(onDataCallback),
+          allowInterop(onPeerLeave),
+          allowInterop(onClose));
+    }
   }
 
   void sendData(String data) {
-    final func = useStab ? stab.sendData : _sendData;
-    func(peerId, data);
+    if (useStab) {
+      stab.sendData(peerId, data);
+    } else {
+      _sendData(data);
+    }
   }
 
   void leaveRoom() {
     final func = useStab ? stab.leaveRoom : _leaveRoom;
-    func(peerId);
+    if (useStab) {
+      stab.leaveRoom(peerId);
+    } else {
+      _leaveRoom();
+    }
   }
 }
