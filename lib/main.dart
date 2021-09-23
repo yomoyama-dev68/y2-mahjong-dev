1f167d2921ee30c99737660186d5c39ec7586897
Your Name <you@example.com>
2021-09-24 01:18:06 +0900

    Fix routing.

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    print("${window.location.href}");
     const locale = Locale("ja", "JP");
     return MaterialApp(
       title: 'Flutter Demo',
@@ -29,36 +28,28 @@ class MyApp extends StatelessWidget {
       supportedLocales: const [
         locale,
       ],
-      onGenerateRoute: RouteGenerator.generateRoute,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: "Noto Sans JP",
       ),
-      home: FirstWidget(roomId: const Uuid().v4()),
+      home: _buildHome(),
     );
   }
-}
 
-class RouteGenerator {
-  static Route<dynamic>? generateRoute(RouteSettings settings) {
-    print("generateRoute: name=${settings.name}, "
-        "arguments=${settings.arguments}, "
-        "${window.location.href}");
-
-    final paths = settings.name!.split('?');
-    final path = paths[0];
-    final queryParameters = Uri.splitQueryString(paths[1]);
-
-    final String? roomId = queryParameters["roomId"];
-    switch (path) {
-      case '/':
-        if (roomId == null) {
-          return MaterialPageRoute(
-              builder: (_) => FirstWidget(roomId: const Uuid().v4()));
-        } else {
-          return MaterialPageRoute(builder: (_) => TopWidget(roomId: roomId));
-        }
+  Widget _buildHome() {
+    print("${window.location.href}");
+    final paths = window.location.href.split('?');
+    if (paths.length <= 1) {
+      return FirstWidget(roomId: const Uuid().v4());
     }
-    return null;
+    final queryParameters = Uri.splitQueryString(paths[1]);
+    print("queryParameters: ${queryParameters}");
+    final String? roomId = queryParameters["roomId"];
+    print("roomId: ${roomId}");
+    if (roomId == null) {
+      return FirstWidget(roomId: const Uuid().v4());
+    } else {
+      return TopWidget(roomId: roomId);
+    }
   }
 }
